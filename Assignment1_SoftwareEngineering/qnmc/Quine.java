8219aeeccc79eaa468c3a8395ce5e12567c9afdd
Emily Wright <emilywright@unknown.home>
2025-02-16 14:09:32 +0000

    refactor quine.java to improve performance and handle unused terms

--- Assignment1_SoftwareEngineering/qnmc/Quine.java
+++ Assignment1_SoftwareEngineering/qnmc/Quine.java
@@ -2,12 +2,13 @@ package qnmc;
 
 public class Quine {
     protected static final int MAX_TERMS = 0xff;
-    public MinTerm[] terms = new MinTerm[MAX_TERMS];
-    public int count = 0;
+    private final MinTerm[] terms = new MinTerm[MAX_TERMS];
+    private int count = 0;
 
+    // Add a new minterm to the list
     public void addMinTerms(String str) throws ExceptionQuine {
         if (count == MAX_TERMS)
-            throw new ExceptionQuine("Quine::addTerm()");
+            throw new ExceptionQuine("Quine::addTerm() - Maximum terms reached.");
         terms[count++] = new MinTerm(str);
     }
 
@@ -20,14 +21,17 @@ public class Quine {
         return buf.toString();
     }
 
+    // Check if a term exists in the list
     public boolean hasTerm(MinTerm a) throws ExceptionQuine {
         for (int i = 0; i < count; i++) {
-            if (a.isSame(terms[i]))
+            if (a.isSame(terms[i])) {
                 return true;
+            }
         }
         return false;
     }
 
+    // Simplify the set of minterms
     public void simplify() throws ExceptionQuine {
         int reductionCount;
         do {
@@ -35,13 +39,16 @@ public class Quine {
         } while (reductionCount > 0);
     }
 
+    // Perform the reduction of terms
     private int reduce() throws ExceptionQuine {
         int reducedCount = 0;
         MinTerm[] reducedTerms = new MinTerm[MAX_TERMS];
         boolean[] used = new boolean[MAX_TERMS];
 
+        // Compare each pair of minterms
         for (int i = 0; i < count; i++) {
             for (int j = i + 1; j < count; j++) {
+                // Check if the minterms differ by exactly one bit
                 if (terms[i].resolutionCount(terms[j]) == 1) {
                     reducedTerms[reducedCount++] = MinTerm.combine(terms[i], terms[j]);
                     used[i] = true;
@@ -50,6 +57,7 @@ public class Quine {
             }
         }
 
+        // Add non-reduced minterms back into the list
         int totalReduced = reducedCount;
         for (int i = 0; i < count; i++) {
             if (!used[i]) {
@@ -57,12 +65,14 @@ public class Quine {
             }
         }
 
+        // Update terms array with reduced terms
         count = 0;
         for (int i = 0; i < totalReduced; i++) {
-            if (!hasTerm(reducedTerms[i]))
+            if (!hasTerm(reducedTerms[i])) {
                 terms[count++] = reducedTerms[i];
+            }
         }
 
-        return reducedCount;
+        return reducedCount;  // Return the number of reduced terms
     }
 }
